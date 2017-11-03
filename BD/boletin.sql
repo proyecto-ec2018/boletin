1339a6da2cb67ff82690547324b76576a839ed41
Victor N <victornoriega>
2017-11-02 17:06:34 -0700

    nueva base de datos

--- BD/boletin.sql
+++ BD/boletin.sql
@@ -2,10 +2,10 @@
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 30-10-2017 a las 04:07:50
--- Versión del servidor: 5.7.19
--- Versión de PHP: 5.6.31
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-11-2017 a las 01:02:36
+-- Versión del servidor: 10.1.26-MariaDB
+-- Versión de PHP: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,19 +25,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `articulos`
+--
+
+CREATE TABLE `articulos` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `autor` varchar(50) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT '0',
+  `descripcion` varchar(300) NOT NULL,
+  `nombre_archivo` varchar(50) NOT NULL,
+  `extension_archivo` varchar(20) NOT NULL,
+  `boletin_asoc` varchar(50) NOT NULL,
+  `fecha_propuesta` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `fecha_respuesta` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+
+--
 -- Estructura de tabla para la tabla `boletines`
 --
 
-DROP TABLE IF EXISTS `boletines`;
-CREATE TABLE IF NOT EXISTS `boletines` (
-  `id_boletin` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `boletines` (
+  `id_boletin` int(11) NOT NULL,
   `nombre_boletin` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `descripcion_boletin` text COLLATE utf8_unicode_ci NOT NULL,
   `creador_boletin` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `fecha_creacion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `es_actual` bit(1) NOT NULL,
-  PRIMARY KEY (`id_boletin`)
+  `es_actual` bit(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `boletines`
+--
 
 -- --------------------------------------------------------
 
@@ -45,19 +66,17 @@ CREATE TABLE IF NOT EXISTS `boletines` (
 -- Estructura de tabla para la tabla `tipo_usuario`
 --
 
-DROP TABLE IF EXISTS `tipo_usuario`;
-CREATE TABLE IF NOT EXISTS `tipo_usuario` (
-  `tipo` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(20) NOT NULL,
-  PRIMARY KEY (`tipo`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+CREATE TABLE `tipo_usuario` (
+  `tipo` int(11) NOT NULL,
+  `description` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `tipo_usuario`
 --
 
 INSERT INTO `tipo_usuario` (`tipo`, `description`) VALUES
-(1, 'user_regist'),
+(1, 'usrRegistrado'),
 (2, 'autor_contenido'),
 (3, 'editor'),
 (4, 'administrador');
@@ -68,15 +87,12 @@ INSERT INTO `tipo_usuario` (`tipo`, `description`) VALUES
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE IF NOT EXISTS `usuarios` (
+CREATE TABLE `usuarios` (
   `userName` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `tipo` int(11) DEFAULT '1',
-  PRIMARY KEY (`userName`),
-  KEY `tipo` (`tipo`)
+  `tipo` int(11) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -84,7 +100,60 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 --
 
 INSERT INTO `usuarios` (`userName`, `email`, `password`, `fecha_registro`, `tipo`) VALUES
-('Victor Noriega', 'victornoriega7@gmail.com', 'c500080b8f612d20946914fcea81dc5a', '2017-10-25 02:05:26', 1);
+('Valoom', 'victornoriega7@gmail.com', 'c500080b8f612d20946914fcea81dc5a', '2017-10-30 02:06:13', 1),
+('victor', 'victornoriega7@gmail.com', 'c500080b8f612d20946914fcea81dc5a', '2017-11-02 06:59:54', 1),
+('Victor Noriega', 'victornoriega7@gmail.com', 'c500080b8f612d20946914fcea81dc5a', '2017-10-25 02:05:26', 4);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `articulos`
+--
+ALTER TABLE `articulos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `boletines`
+--
+ALTER TABLE `boletines`
+  ADD PRIMARY KEY (`id_boletin`);
+
+--
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`tipo`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`userName`),
+  ADD KEY `tipo` (`tipo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `articulos`
+--
+ALTER TABLE `articulos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `boletines`
+--
+ALTER TABLE `boletines`
+  MODIFY `id_boletin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  MODIFY `tipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
