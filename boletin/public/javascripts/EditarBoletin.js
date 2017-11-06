f285454d1d3f116f247fd78b0c75b20d65775d31
fabian6 <fabian6_03@hotmail.com>
2017-11-05 19:57:59 -0800

    Se agrego la vista, css y js de Editar boletin

---
+++ boletin/public/javascripts/EditarBoletin.js
@@ -0,0 +1,14 @@
+function alertaBoletin(){
+  var respuesta = confirm("Estas seguro de borrar el boletin?");
+  if(respuesta){
+    alert("El boletin se ha borrado");
+  }
+
+}
+
+function alertaArticulo(){
+  var respuesta = confirm("Estas seguro de borrar el articulo?");
+  if(respuesta){
+    alert("El articulo se ha borrado");
+  }
+}
